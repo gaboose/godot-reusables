2f9a3801dda9c76bbb25dcd2b27233669f49850c
Gabrielius Mickevičius <gabrielius.m@gmail.com>
2026-06-19 18:15:41 +0300

    Fix capture_mouse

--- capture_mouse/capture_mouse.gd
+++ capture_mouse/capture_mouse.gd
@@ -5,10 +5,13 @@ func _ready():
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-
-	if event.is_action_pressed("ui_select"):
-		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	if event is InputEventMouseButton and event.pressed:
+		if Input.mouse_mode != Input.MOUSE_MODE_CAPTURED:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 			get_viewport().set_input_as_handled()
 	
