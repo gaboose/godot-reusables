23d317002a5f655d0a6e591fd041e43b6d901f1f
Gabrielius Mickevičius <gabrielius.m@gmail.com>
2026-07-08 18:10:29 +0300

    Add sprint and make jump better

--- player/player.gd
+++ player/player.gd
@@ -1,15 +1,23 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
-const ACCELERATION = 50.0
-const JUMP_ACCELERATION = 10.0
-const JUMP_VELOCITY = 4.5
+const ACCELERATION = 60.0
+const FRICTION = 15.0
+const SPRINT = 1.6
+const JUMP_ACCELERATION = 3.0
+const JUMP_IMPULSE = 4.5
+const JUMP_FRICTION = 0.01
+
+const SPEED = ACCELERATION / FRICTION
+const SPRINT_SPEED = SPEED * SPRINT
 
 const MOUSE_SENSITIVITY = 0.002
 const RIGHT_JOYSTICK_SENSITIVITY = 3.5;
 
 var smoothed_look_input := Vector2.ZERO
 const SMOOTHING := 16.0
+
+var sprint_jump := false
+var double_jump := true
 
 func _action_add_key_event(action, keycode):
 	var event = InputEventKey.new()
@@ -50,6 +58,10 @@ func _ready() -> void:
 	_action_add_key_event("move_jump", KEY_SPACE)
 	_action_add_joypad_button_event("move_jump", JOY_BUTTON_A)
 	
+	InputMap.add_action("move_sprint")
+	_action_add_key_event("move_sprint", KEY_SHIFT)
+	_action_add_joypad_button_event("move_sprint", JOY_BUTTON_RIGHT_STICK)
+	
 	InputMap.add_action("look_left")
 	_action_add_joypad_motion_event("look_left", JOY_AXIS_RIGHT_X, -1.0)
 	
@@ -64,8 +76,14 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("move_jump"):
-		velocity.y = JUMP_VELOCITY
+		if double_jump:
+			velocity.y = JUMP_IMPULSE
+			if is_on_floor():
+				sprint_jump = Input.is_action_pressed("move_sprint")
+			else:
+				double_jump = false
 		get_viewport().set_input_as_handled()
+				
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
@@ -75,6 +93,8 @@ func _unhandled_input(event):
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		double_jump = true
 	
 	var look_input = Vector2(
 		Input.get_action_strength("look_right") - Input.get_action_strength("look_left"),
@@ -86,17 +106,29 @@ func _physics_process(delta: float) -> void:
 	rotate_y(-smoothed_look_input.x * RIGHT_JOYSTICK_SENSITIVITY * delta)
 	rotate_object_local(Vector3(-1, 0, 0), smoothed_look_input.y * RIGHT_JOYSTICK_SENSITIVITY * delta)
 
-	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var direction := transform.basis * Vector3(input_dir.x, 0, input_dir.y)
+	var move_input := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var move_direction := transform.basis * Vector3(move_input.x, 0, move_input.y)
+	move_direction.y = 0.0
+	move_direction = move_direction.normalized()
 	
-	if direction:
-		var acceleration = (ACCELERATION if is_on_floor() else JUMP_ACCELERATION) * delta * direction
-		velocity.x += acceleration.x
-		velocity.z += acceleration.z
-		velocity = velocity.limit_length(direction.length() * SPEED)
-	elif is_on_floor():
-		var acceleration = ACCELERATION * delta;
-		velocity.x = move_toward(velocity.x, 0, acceleration)
-		velocity.z = move_toward(velocity.z, 0, acceleration)
+	var hvel = Vector2(velocity.x, velocity.z)
+	var friction = (FRICTION if is_on_floor() else JUMP_FRICTION)
+	hvel.x = move_toward(hvel.x, 0, abs(hvel.x) * friction * delta)
+	hvel.y = move_toward(hvel.y, 0, abs(hvel.y) * friction * delta)
+	
+	if move_direction:
+		var acceleration = (ACCELERATION if is_on_floor() else JUMP_ACCELERATION)
+		if Input.is_action_pressed("move_sprint"):
+			acceleration *= SPRINT
+		
+		var acceleration_vector = acceleration * move_direction
+		hvel.x += acceleration_vector.x * delta
+		hvel.y += acceleration_vector.z * delta
+	
+	if !is_on_floor():
+		hvel = hvel.limit_length(SPRINT_SPEED if sprint_jump else SPEED)
+	
+	velocity.x = hvel.x
+	velocity.z = hvel.y
 
 	move_and_slide()
